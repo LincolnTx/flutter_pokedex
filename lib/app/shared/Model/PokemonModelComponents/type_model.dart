dc25d030663b3b36f9238d6f6f06559d8ea7775b
lincolntx <lincolnsf98@gmail.com>
2020-02-24 17:19:35 -0300

    missing file in last commit

---
+++ lib/app/shared/Model/PokemonModelComponents/type_model.dart
@@ -0,0 +1,18 @@
+class Type {
+  String name;
+  String url;
+
+  Type({this.name, this.url});
+
+  Type.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    url = json['url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['url'] = this.url;
+    return data;
+  }
+}
