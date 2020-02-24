3f84537b03b3f72193daf2021acd0da49ff18284
lincolntx <lincolnsf98@gmail.com>
2020-02-24 16:49:40 -0300

    creating enum for pokemon types, and adding a new const for colors

--- lib/app/shared/Model/constants.dart
+++ lib/app/shared/Model/constants.dart
@@ -1,5 +1,27 @@
+import 'package:pokedex/app/shared/Model/pokemon_types.dart';
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 // add the pokemon index on this two urls
 const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/";
 const POKEMON_SPECIES_URL = "https://pokeapi.co/api/v2/pokemon-species/";
+
+const TYPE_COLORS = {
+  PokemonTypes.bug:'BCCA21',
+  PokemonTypes.dark:'8C6855',
+  PokemonTypes.dragon:'8A76FF',
+  PokemonTypes.electric:'FDE53C',
+  PokemonTypes.fairy:'FAADFF',
+  PokemonTypes.fighting:'A85644',
+  PokemonTypes.fire:'FA5543',
+  PokemonTypes.flying:'79A4FF',
+  PokemonTypes.ghots:'7975D6',
+  PokemonTypes.grass:'8CD751',
+  PokemonTypes.ground:'EDCC56',
+  PokemonTypes.ice:'96F1FF',
+  PokemonTypes.normal:'BABAAC',
+  PokemonTypes.poison:'AB5DA3',
+  PokemonTypes.psychic:'F662B2',
+  PokemonTypes.rock:'CDBD72',
+  PokemonTypes.steel:'C3C1D8',
+  PokemonTypes.water:'55ACFF'
+};
---
+++ lib/app/shared/Model/pokemon_types.dart
@@ -0,0 +1,21 @@
+enum PokemonTypes {  
+  bug,
+  dark,
+  dragon,
+  electric,
+  fairy,
+  fighting,
+  fire,
+  flying,
+  ghots,
+  grass,
+  ground,
+  ice,
+  normal,
+  poison,
+  psychic,
+  rock,
+  steel,
+  water,
+  
+}
